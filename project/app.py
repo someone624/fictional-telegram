d0eed1a255f81ca93b5dd4924cadcf588ebb8451
someone624 <anamikahobby@gmail.com>
2024-09-19 21:20:18 +0200

    Update app.py

--- project/app.py
+++ project/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 st.title('hello world')
 time.sleep(5)
@@ -16,6 +17,12 @@ st.write(f'Hello, {name}!')
 
 st.text_input('Enter your text here:')
 st.slider('Pick a number', 0, 100)
+
+st.image('assets/example_image.png', caption='Example Image')
+
+data = pd.read_csv('data/example_data.csv')
+st.write('Here is some example data:')
+st.dataframe(data)
 
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
